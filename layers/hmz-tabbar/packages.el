a7d59d86ed68e9aba39186043e0aa45790b1f42a
Fabio Mazarotto <fabio@mazarotto.me>
2017-08-07 12:04:40 -0300

    Tabbar tabs are pratically done. TODO: tabbar buttons!

--- layers/hmz-tabbar/packages.el
+++ layers/hmz-tabbar/packages.el
@@ -19,7 +19,7 @@ which require an initialization must be listed explicitly in the list.")
      '(tabbar-separator (quote (1.2))))
 
     (custom-set-faces
-     '(tabbar-default ((t (:height 1.0 :overline t :background "#2E3440" :weight thin :foreground "#AAAAAA" :family "San Francisco"))))
+     '(tabbar-default ((t (:height 1.0 :background "#2E3440" :weight thin :foreground "#AAAAAA" :family "San Francisco"))))
      '(tabbar-modified ((t (:inherit tabbar-default :foreground "SeaGreen"))))
      '(tabbar-button ((t (:inherit tabbar-default))))
      '(tabbar-selected ((t (:inherit tabbar-default :overline t :foreground "white"))))
@@ -35,13 +35,10 @@ which require an initialization must be listed explicitly in the list.")
       "Return a label for TAB.
 That is, a string used to represent it on the tab bar."
 
-      (let ((label
-             (if tabbar--buffer-show-groups
-                 (replace-regexp-in-string "*" ""
-                  (format "%s"
-                          (tabbar-tab-tabset tab)))
-               (format "%s" (tabbar-tab-value tab))
-               )))
+      (let ((label (if tabbar--buffer-show-groups
+                       (replace-regexp-in-string
+                        "*" "" (format "%s" (tabbar-tab-tabset tab)))
+                     (format "%s" (tabbar-tab-value tab)))))
         ;; Unless the tab bar auto scrolls to keep the selected tab
         ;; visible, shorten the tab label to keep as many tabs as possible
         ;; in the visible area of the tab bar.
@@ -58,10 +55,44 @@ That is, a propertized string used as an `header-line-format' template
 element.
 Call `tabbar-tab-label-function' to obtain a label for TAB."
 
+      ;; (setq j (all-the-icons-icon-for-file "jujuba.rb"))
+      ;; (set-text-properties 0 0 nil i)
+      ;; (message "%s - %s" i (text-properties-at 0 i))
+
+      ;; (remove-text-properties 0 0 '(face nil) i)
+      ;; (message "%s" (plist-put (plist-get (text-properties-at 0 j) 'face) :height 0.5))
+
+      ;; (message "%s" i)
+      ;; (message "%s"
+      ;;          (propertize i
+      ;;                      'face (:inherit (text-properties-at 0 i))))
+
+
+      (setq
+       tab-face (cond ((and (tabbar-selected-p tab (tabbar-current-tabset))
+                            (tabbar-modified-p tab (tabbar-current-tabset)))
+                       'tabbar-selected-modified)
+                      ((tabbar-selected-p tab (tabbar-current-tabset))
+                       'tabbar-selected)
+                      ((tabbar-modified-p tab (tabbar-current-tabset))
+                       'tabbar-modified)
+                      (t 'tabbar-unselected))
+
+       the-icon (all-the-icons-icon-for-file
+                 (replace-regexp-in-string "<.*>" "" (format "%s"(tabbar-tab-value tab)))))
+
+
+      (setq icon-face
+            (plist-put (plist-get (text-properties-at 0 the-icon) 'face) :height 1.0))
+
       (concat
-       (all-the-icons-icon-for-file
-        (replace-regexp-in-string "<.*>" "" (format "%s"(tabbar-tab-value tab)))
+
+       (propertize
+        the-icon
+        'face icon-face
+        'display '(raise 0.3)
         )
+       " "
        (propertize
         (concat
          (if tabbar-tab-label-function
@@ -71,25 +102,15 @@ Call `tabbar-tab-label-function' to obtain a label for TAB."
         'local-map (tabbar-make-tab-keymap tab)
         'help-echo 'tabbar-help-on-tab
         'mouse-face 'tabbar-highlight
-        'face (cond ((and (tabbar-selected-p tab (tabbar-current-tabset))
-                          (tabbar-modified-p tab (tabbar-current-tabset)))
-                     'tabbar-selected-modified)
-                    ((tabbar-selected-p tab (tabbar-current-tabset))
-                     'tabbar-selected)
-                    ((tabbar-modified-p tab (tabbar-current-tabset))
-                     'tabbar-modified)
-                    (t 'tabbar-unselected))
-        'display '(raise 0.2)
+        'face tab-face
+        'display '(raise 0.3)
         'pointer 'hand)
        tabbar-separator-value))
-
-
 
     (defsubst tabbar-line-button (name)
       "Return the display representation of button NAME.
 That is, a propertized string used as an `header-line-format' template
 element."
-      (message "%s" name)
       (let ((label (if tabbar-button-label-function
                        (funcall tabbar-button-label-function name)
                      (cons name name))))
@@ -104,7 +125,7 @@ element."
                              ((eq name 'scroll-right) (all-the-icons-wicon "hurricane"))
                              (t "X")) " ")
                'tabbar-button name
-               'face `(:inherit 'tabbar-default :family ,(all-the-icons-wicon-family) :height 1.5 :foreground "orange")
+               'face `(:inherit tabbar-default :family ,(all-the-icons-wicon-family) :height 1.5 :foreground "orange")
                'display '(raise 0.0)
                'mouse-face 'tabbar-button-highlight
                'pointer 'hand
@@ -118,7 +139,7 @@ element."
                              ((eq name 'scroll-right) (all-the-icons-wicon "hurricane"))
                              (t "X")) " ")
                'tabbar-button name
-               'face `(:inherit 'tabbar-default :family ,(all-the-icons-wicon-family) :height 1.5 :foreground "#AAAAAA")
+               'face `(:inherit tabbar-default :family ,(all-the-icons-wicon-family) :height 1.5 :foreground "#AAAAAA")
                'display '(raise 0.0)
                'mouse-face 'tabbar-button-highlight
                'pointer 'hand
