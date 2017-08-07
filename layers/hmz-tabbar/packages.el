ded19d8b4614d61d9dd16acf8a4adf4856c57d6e
Fabio Mazarotto <fabio@mazarotto.me>
2017-08-06 23:25:18 -0300

    tabbar and etc

--- layers/hmz-tabbar/packages.el
+++ layers/hmz-tabbar/packages.el
@@ -19,12 +19,13 @@ which require an initialization must be listed explicitly in the list.")
      '(tabbar-separator (quote (1.2))))
 
     (custom-set-faces
-     '(tabbar-default ((t (:height 1.2 :overline t :weight thin :family "San Francisco"))))
+     '(tabbar-default ((t (:height 1.0 :overline t :background "#2E3440" :weight thin :foreground "#AAAAAA" :family "San Francisco"))))
      '(tabbar-modified ((t (:inherit tabbar-default :foreground "spring green"))))
      '(tabbar-button ((t (:inherit tabbar-default))))
-     '(tabbar-selected ((t (:inherit tabbar-default :overline "deep sky blue" :foreground "deep sky blue"))))
+     '(tabbar-selected ((t (:inherit tabbar-default :overline t :foreground "white"))))
 
-     '(tabbar-selected-modified ((t (:inherit tabbar-selected :foreground "spring green" :background "#346B47"))))
+     '(tabbar-selected-modified ((t (:inherit tabbar-selected :foreground "spring green"))))
+
      '(tabbar-unselected ((t (:inherit tabbar-default )))))
 
 
@@ -56,43 +57,65 @@ That is, a string used to represent it on the tab bar."
 That is, a propertized string used as an `header-line-format' template
 element.
 Call `tabbar-tab-label-function' to obtain a label for TAB."
+
+      (setq jj "mehsdfkjalkdjflkasdjf")
       (concat
-       (propertize
-        (concat
-         (all-the-icons-icon-for-file
-          (replace-regexp-in-string "<.*>" "" (format "%s"(tabbar-tab-value tab))))
-         " "
-         )
-        'face `(:inherit ,(cond ((and (tabbar-selected-p tab (tabbar-current-tabset))
-                                      (tabbar-modified-p tab (tabbar-current-tabset)))
-                                 'tabbar-selected-modified)
-                                ((tabbar-selected-p tab (tabbar-current-tabset))
-                                 'tabbar-selected)
-                                ((tabbar-modified-p tab (tabbar-current-tabset))
-                                 'tabbar-modified)
-                                (t 'tabbar-unselected))  :family ,(all-the-icons-octicon-family)
-                                :foreground 'tabbar-selected
-                                :overline t
-                                :height 1.3)
-        'display '(raise -0.2))
-       (propertize
-        (if tabbar-tab-label-function
-            (funcall tabbar-tab-label-function tab)
-          tab)
-        'tabbar-tab tab
-        'local-map (tabbar-make-tab-keymap tab)
-        'help-echo 'tabbar-help-on-tab
-        'mouse-face 'tabbar-highlight
-        'face (cond ((and (tabbar-selected-p tab (tabbar-current-tabset))
-                          (tabbar-modified-p tab (tabbar-current-tabset)))
-                     'tabbar-selected-modified)
-                    ((tabbar-selected-p tab (tabbar-current-tabset))
-                     'tabbar-selected)
-                    ((tabbar-modified-p tab (tabbar-current-tabset))
-                     'tabbar-modified)
-                    (t 'tabbar-unselected))
-        'pointer 'hand)
+        (propertize
+           (concat
+            (all-the-icons-icon-for-file
+             (replace-regexp-in-string "<.*>" "" (format "%s"(tabbar-tab-value tab)))
+             )
+            (if tabbar-tab-label-function
+                (funcall tabbar-tab-label-function tab)
+              tab))
+           'tabbar-tab tab
+           'local-map (tabbar-make-tab-keymap tab)
+           'help-echo 'tabbar-help-on-tab
+           'mouse-face 'tabbar-highlight
+           'face (cond ((and (tabbar-selected-p tab (tabbar-current-tabset))
+                             (tabbar-modified-p tab (tabbar-current-tabset)))
+                        'tabbar-selected-modified)
+                       ((tabbar-selected-p tab (tabbar-current-tabset))
+                        'tabbar-selected)
+                       ((tabbar-modified-p tab (tabbar-current-tabset))
+                        'tabbar-modified)
+                       (t 'tabbar-unselected))
+           'display '(raise 0.1)
+           'pointer 'hand)
+        (put-text-property 1 3 '(:family "San Francisco") jj)
+        jj
        tabbar-separator-value))
+
+
+
+    (defsubst tabbar-line-button (name)
+      "Return the display representation of button NAME.
+That is, a propertized string used as an `header-line-format' template
+element."
+      (message "%s" name)
+      (let ((label (if tabbar-button-label-function
+                       (funcall tabbar-button-label-function name)
+                     (cons name name))))
+        ;; Cache the display value of the enabled/disabled buttons in
+        ;; variables `tabbar-NAME-button-value'.
+        (set (intern (format "tabbar-%s-button-value"  name))
+             (cons
+              (propertize
+               (concat " "
+                       (cond ((eq name 'home) (all-the-icons-wicon "alien"))
+                             ((eq name 'scroll-left) (all-the-icons-wicon "fire"))
+                             ((eq name 'scroll-right) (all-the-icons-wicon "hurricane"))
+                             (t "X")) " ")
+                          'tabbar-button name
+                          'face `(:inherit 'tabbar-default :family ,(all-the-icons-wicon-family) :height 1.5 :foreground "orange")
+                          'display '(raise 0.0)
+                          'mouse-face 'tabbar-button-highlight
+                          'pointer 'hand
+                          'local-map (tabbar-make-button-keymap name)
+                          'help-echo 'tabbar-help-on-button)
+              (propertize (cdr label)
+                          'face 'tabbar-button
+                          'pointer 'arrow)))))
 
 
 
